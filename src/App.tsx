df52812a2e5c9bf0366171f9480e75643a239a70
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-09 18:07:07 +0530

    login-register

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+import {RegisterPage} from "./RegisterPage";
+import {LoginPage} from './LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+      <Switch>
+          <Route path="/login" component={LoginPage}></Route>
+          <Route path="/register" component={RegisterPage}></Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
