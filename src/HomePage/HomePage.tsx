ad8b7a41b02b3a8202c3734ac669f2a118450e59
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-23 18:39:54 +0530

    modal create

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -2,19 +2,16 @@
 import userService from "../services/user-service";
 import { useState,useEffect} from "react";
 import { useHistory } from "react-router-dom";
-import {Table,Button,Switch,Layout,Typography} from "antd";
-
-
+import {Table,Button,Switch,Layout,Typography,Modal} from "antd";
 import {
     UsergroupAddOutlined,
     PlusCircleTwoTone,
   } from '@ant-design/icons';
+import CustomeModal from "./CustomModal";
 
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
   
-  const { Header, Content, Footer } = Layout;
-  const { Title } = Typography;
-  
-
 interface postData{
     id: number;
     data: string;
@@ -22,11 +19,12 @@ interface postData{
     priority: number;
 }
 
-
 export default function HomePage() {
     const [posts, setPosts] = useState([]);
     const history= useHistory(); 
-    
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [id, setId]= useState(null);
+
     useEffect(() => {
         userService.getAddData().then(
           (response) => {
@@ -53,68 +51,61 @@ export default function HomePage() {
             })
     }
     const onDelete = (id:any) => {
+        console.log(id);
         userService.DeleteData(id)
             .then(() => {
                 getData();
             })
+            setIsModalVisible(false);
     }
-    // const columns = [
-    //     {
-    //         key:'1',
-    //         title:'ID',
-    //         dataIndex:'id',
-    //         sorter: true,
-    //         // sorter: () =>  onSort /* (a:any, b:any) => a.id - b.id */
-    //     },
-    //     {
-    //         key:'2',
-    //         title:'Data',
-    //         dataIndex:'data',
-    //         sorter: true,
-    //         // sorter: (a:any, b:any) => a.data.localeCompare(b.data)
-    //     },
-    //     {
-    //         key:'3',
-    //         title:'Date',
-    //         dataIndex:'due_date',
-    //         sorter: true,
-    //         // sorter: (a:any, b:any) => a.due_date.localeCompare(b.due_date)
-    //     },
-    //     {
-    //         key:'4',
-    //         title:'Priority',
-    //         dataIndex:'priority',
-    //         sorter: true,
-    //         // sorter: (a:any, b:any) => a.priority - b.priority
-    //     },
-    //     {
-    //         key:'5',
-    //         title:'Complete',
-    //         dataIndex:'',
-    //         render: () => <Switch />
-    //     },
-    //     {
-    //         key:'6',
-    //         title:'Delete',
-    //         dataIndex:'id', 
-    //         render: (id:any) => <Button onClick={() => onDelete(id)}>Delete</Button>,
-    //     },
-    // ]
+    const showModal = (id:any) => {
+        console.log(id);
+        
+        setId(id);
+        setIsModalVisible(true);
+      };
+      const handleCancel = () => {
+        setIsModalVisible(false);
+      };
 
     const columns = [
         {
+          key:'id',
           title: 'Id',
           dataIndex: 'id',
           sorter: true,
-          width: '20%',
+          width: '10%',
         },
         {
-            key:'2',
+            key:'data',
             title:'Data',
             dataIndex:'data',
             sorter: true,
         },
-        
+        {
+            key:'due_date',
+            title:'Date',
+            dataIndex:'due_date',
+            sorter: true,
+        },
+        {
+            key:'priority',
+            title:'Priority',
+            dataIndex:'priority',
+            sorter: true,
+        },
+        {
+            key:'x',
+            title:'Complete',
+            dataIndex:'',
+            render: () => <Switch />
+        },
+        {
+            key:'id',
+            title:'Delete',
+            dataIndex:'id', 
+            render: (id:any) => <Button onClick={() => showModal(id)}>Delete</Button>,
+        },
     ];
 
     const nextpath = (path: any) => {
@@ -122,16 +113,21 @@ export default function HomePage() {
       };
     
     const handleTableChange = (pagination: any, filters: any, sorter: any) => {
+      
         userService.SortData(sorter.field, sorter.order === "ascend" ? 'asc' : 'desc')
+      
         .then((getData) => {
             setPosts(getData.data.data.todos);
         })
+        console.log("field",sorter.field);
+        console.log("order",sorter.order);
+        
     }
 
     return (
     <div>
         <Layout >
-        <Header style={{ backgroundColor:"#D8D4D5"}} >Home Page
+        <Header style={{ backgroundColor:"#D8D4D5"}} >Home Page 
             <Content style={{ padding: '0 50px' }}>Todos
             <Title style={{color:'#08c'}}><UsergroupAddOutlined style={{ fontSize: '30px', color: '#08c' }}/> 
                 ToDos List <PlusCircleTwoTone twoToneColor= "#ff0000" style={{ fontSize: '25px' }}  onClick={() => nextpath("/add_data")}/></Title>   
@@ -140,8 +136,18 @@ export default function HomePage() {
                     dataSource={posts}
                     onChange={(pagination, filters, sorter) => handleTableChange(pagination, filters, sorter)}
                 />
-
             </Content>
+
+            <CustomeModal
+                title="Delete the data"
+                visible={isModalVisible} 
+                onOk={() => onDelete(id)} 
+                onCancel={handleCancel}
+                modalBody={<h2 style={{color: "#08c"}}> Are You Sure?</h2>}
+            />
+            {/* <Modal title="Delete the data" visible={isModalVisible} onOk={() => onDelete(id)} onCancel={handleCancel}>
+                <h2 style={{color: "#08c"}}> Are You Sure ?</h2>
+            </Modal> */}
         <Footer></Footer>
         </Header>
         </Layout>
